b6b736e9374ff8afe0a3c07856f13bb2e1cfe023
ganil-kumar <g.anilkumar3852@gmail.com>
2026-02-08 20:07:40 +0530

    intial commit

--- roboshop.sh
+++ roboshop.sh
@@ -5,24 +5,24 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do 
-    instance_id = $( aws ec2 run-instances \
+    INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_ID}]" \
     --query 'Instances[0].InstanceId' \
     --output text )
 
-    if [ $instance == "frontend" ]; then
+    if [ $INSTANCE_ID == "frontend" ]; then
         IP=$(
             aws ec2 describe-instances \
-            --instance-ids $instance_id \
+            --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
         )
     else 
         IP=$(aws ec2 describe-instances \
-            --instance-ids $instance_id \
+            --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
         )
